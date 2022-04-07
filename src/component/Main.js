c94db7ba76a03a94fec251ae834ac67952bcd197
bini-man <biniyambadege4@gmail.com>
2022-04-07 22:01:06 +0300

    every thing has been done except when the user try to start the timer again

--- src/component/Main.js
+++ src/component/Main.js
@@ -1,114 +1,86 @@
 import Timer from "./Timer";
 import './Main.css'
 import React, { useEffect, useState } from 'react'
-import { TextField, Typography } from '@mui/material'
+import { TextField, Typography,Button, IconButton } from '@mui/material'
 import {  PauseCircle, PlayCircleFilledWhite } from '@mui/icons-material';
-import { Button, IconButton } from '@mui/material';
-import { makeStyles } from '@mui/styles';
 function Main() {
   const [minute,setMinute]=useState(0)
-
-const [timer,setTimer]=useState({sec:0,min:minute})
-
-const [interv,setInterv]=useState()
-const [status,setStatus]=useState(3)
-const [speed,setSpeed]=useState({speed:1000})
-var updatedspeed = speed.speed;
-const useStyle=makeStyles({
-        button: {
-              backgroundColor:'#54B8A1'
-          }
-  });
+  const [timer,setTimer]=useState({sec:0,min:minute})
+  const [interv,setInterv]=useState()
+  const [status,setStatus]=useState(3)
+  const [speed,setSpeed]=useState({speed:1000})
+  const [disabel,setDisable]=useState(true)
+  const [changed,setChanged]=useState(false)
   const style={
-    backgroundColor:'#4A4A4A'
+        backgroundColor:'#4A4A4A'
   }
-const start = (spe)=>{
-    clearInterval(interv)
-  if(updatedmin==0&&updatedsec==0) return
- Math.floor(updatedmin)
-  setStatus(1)
- 
-  run();
-  setInterv(setInterval(run,spe))
+  const start = (spe)=>{
+        clearInterval(interv)
+        if(updatedmin===0&&updatedsec===0) return
+        Math.round(updatedmin)
+        setStatus(1)
+        run();
+        setInterv(setInterval(run,spe))   
 }
-useEffect(()=>{
-  if(updatedmin===0 && updatedsec===0){
-    clearInterval(interv)
+  useEffect(()=>{
+         if(updatedmin===0 && updatedsec===0){
+            clearInterval(interv)
   }
 })
-
-const run = () =>{
-  if(updatedsec===0){
-    updatedmin--;
-    updatedsec=60
-  }
-  if(updatedmin<=0 && updatedsec===0){
-    clearInterval(interv)
-  }
-  updatedsec--;
-  return setTimer({sec:updatedsec,min:updatedmin});
+  const run = () =>{
+        if(updatedsec===0){
+                updatedmin--;
+                updatedsec=60
+        }
+        if(updatedmin<=0 && updatedsec===0){
+               clearInterval(interv)
+        }
+        updatedsec--;
+        return setTimer({sec:updatedsec,min: Math.round(updatedmin)});
 }
-
-const speed_interval = (sp) =>{
-
-  clearInterval(interv)
-  setSpeed({speed:sp})
- start(sp)
+  const speed_interval = (sp) =>{
+        clearInterval(interv)
+        setSpeed({speed:sp})
+        start(sp)
 }
-const stop = () =>{
-  setStatus(2)
-  clearInterval(interv)
+  const stop = () =>{
+        setDisable(true)
+        setStatus(2)
+        clearInterval(interv)
 }
-const resume = () =>{
-  setStatus(1)
-  start(1000)
+  const resume = () =>{
+        setDisable(false)
+        setStatus(1)
+        start(speed.speed)
 }
-var updatedsec = timer.sec , updatedmin = timer.min;
-const classes=useStyle()
-
+  var updatedsec = timer.sec , updatedmin = timer.min;
   return (
     <div className="App">
-        <section id="first-col" >
-       <Typography id='countdown' variant="h5" component="h2">
-  Countdown:
-</Typography><TextField id="value" label="(MIN)"  value={minute} onChange={(e)=>
-  {
-   
-   (e.target.value<0)? setMinute(1):
-  setMinute(e.target.value)
-  
-}
-}
- type="number" variant="outlined"  />
-
-   <button id="button" onClick={()=>{
-          updatedmin=minute
-          start(1000)
-         
-        }} >Start</button>
-     
-     </section>
-    <Timer timer={timer} min={minute} updatedmin={updatedmin} status={status} updatedsec={updatedsec}/>
-   <section id="control">
-    {(status==1) ? <IconButton onClick={stop} >
-    <PauseCircle color='inherit' sx={{ fontSize: 40 }} />
-</IconButton>:""}
-{(status==2) ? <IconButton onClick={resume}>
-    <PlayCircleFilledWhite color='inherit' sx={{ fontSize: 40 }} />
-</IconButton>:""}
-<Button style={(speed.speed==1000)? style:null} variant='outlined' onClick={()=>
-  { 
-  speed_interval(1000)}}>1x</Button>
-<Button style={(speed.speed==500)? style:null}  variant='outlined' onClick={()=> { 
-
-  speed_interval(500)
-}}>1.5x</Button>
-<Button style={(speed.speed==100)? style:null}  variant='outlined' onClick={()=> { 
-
- speed_interval(100)}}>2x</Button>
- </section>
+       <section id="first-col" >
+          <Typography id='countdown' variant="h5" component="h2">Countdown:</Typography>
+          <TextField id="value" label="(MIN)"  value={minute} onChange={(e)=>
+              {
+                      (e.target.value<0)? setMinute(1):
+                      setMinute(e.target.value)
+              }
+            } type="number" variant="outlined"  />
+          <button id="button" onClick={()=>
+              {
+                      if(status===1) setChanged(true)
+                      updatedmin=minute
+                      start(speed.speed)
+                      setDisable(false)
+              }} >Start</button>    
+       </section>
+         <Timer timer={timer} min={minute} changed={changed} updatedmin={updatedmin} status={status} updatedsec={updatedsec}/>
+       <section id="control">
+           {(status===1) ? <IconButton onClick={stop} ><PauseCircle color='inherit' sx={{ fontSize: 40 }} /></IconButton>:""}
+           {(status===2) ? <IconButton onClick={resume}><PlayCircleFilledWhite color='inherit' sx={{ fontSize: 40 }} /></IconButton>:""}
+            <Button style={(speed.speed===1000)? style:null} disabled={disabel} variant='outlined' onClick={()=>speed_interval(1000)}>1x</Button>
+            <Button style={(speed.speed===500)? style:null}  disabled={disabel} variant='outlined' onClick={()=> speed_interval(500)}>1.5x</Button>
+            <Button style={(speed.speed===100)? style:null}  disabled={disabel} variant='outlined' onClick={()=> speed_interval(100)}>2x</Button>
+       </section>
     </div>
   );
 }
-
 export default Main;
--- src/component/Timer.js
+++ src/component/Timer.js
@@ -1,65 +1,60 @@
 import './Timer.css'
-import {   Typography } from '@mui/material'
+import {  Typography } from '@mui/material'
 import React, { useEffect, useState } from 'react';
 import { makeStyles } from '@mui/styles';
 export default function Timer(props) {
-  
-  const [texts,setTexts]=useState('')
-  const[text_color,setText_color]=useState('black')
-  const [display,setDisplay]=useState('none')
-  const [displayOrg,setDisplayOrg]=useState('')
-
-  const style={
-    color:'#FF0000',
-    display:display,
-    
-  }
-  const styleOrginal={
-    display:displayOrg
-  }
-  const useStyle=makeStyles((theme)=>{
-    return{
-      root:{
-        display: "flex",
-        color:text_color
+      const [texts,setTexts]=useState('')
+      const [text_color,setText_color]=useState('black')
+      const [display,setDisplay]=useState('none')
+      const [displayOrg,setDisplayOrg]=useState('')
+      let ch=props.changed
+      const style={
+        color:'#FF0000',
+        display:display,
+      }
+      const styleOrginal={
+        display:displayOrg
+      }
+      const useStyle=makeStyles((theme)=>{
+        return{
+          root:{
+            display: "flex",
+            color:text_color
+              }
+          }
+      })
+      const classes=useStyle()
+      useEffect(()=>{
+            if(props.min===1&&props.updatedsec===30){
+              setTexts("More than half way there!")
             }
-
-  }
-  })
-  const classes=useStyle()
-  useEffect(()=>{
-    if(props.min==1&&props.updatedsec==30){
-      setTexts("More than half way there!")
-    }
-    if(props.min/2>=props.updatedmin && props.updatedmin!=0&&props.updatedsec==0){
-      setTexts("More than half way there!")
-    }
-    if(props.updatedmin<=0&&props.updatedsec==0&&props.min!=0&&props.status!=3){
-      setTexts("Time's Up!!")
-    }
-    if(props.updatedmin==0&&props.updatedsec==20){
-      setText_color('red')
-    }
-    if(props.updatedmin==0&&props.updatedsec <= 10&&props.status!=3){
-    setDisplay('')
-    setDisplayOrg('none')
-    }
-  })
+            if(props.min/2>=props.updatedmin && props.updatedmin!==0&&props.updatedsec===0){
+              setTexts("More than half way there!")
+            }
+            if(props.updatedmin<=0&&props.updatedsec===0&&props.min!==0&&props.status!==3){
+              setTexts("Time's Up!!")
+            }
+            if(props.updatedmin===0&&props.updatedsec===20){
+              setText_color('red')
+            }
+            if(props.updatedmin===0&&props.updatedsec <= 10&&props.status!==3){
+              setDisplay('')
+              setDisplayOrg('none')
+            }
+            if(ch===true){
+                setTexts("")
+                setText_color("black")
+                setDisplay('none')
+                ch=false
+            }
+  },[props.updatedsec,props.min, props.status,props.updatedmin])
  return (
     <div>
-    
-<Typography id="notification" variant="h5" component="h2"> { texts }</Typography>
-<section id='counter'>
-<Typography style={styleOrginal} fontWeight="fontWeightBold" variant="h1" component="h2" className={classes.root}>{(props.timer.min<10)? <Typography fontWeight="fontWeightBold" variant="h1" component="h2" className={classes.root}>0</Typography>:null} 
-{props.timer.min}:{(props.timer.sec<10)? 
-<Typography fontWeight="fontWeightBold" variant="h1" component="h2" className={classes.root}>0</Typography>:
-null} {props.timer.sec}</Typography>
-
-   <Typography id='blink' style={style} fontWeight="fontWeightBold" variant="h1" component="h2" className={classes.root}>{(props.timer.min<10)? <Typography style={style} fontWeight="fontWeightBold" variant="h1" component="h2" className={classes.root}>0</Typography>:null} 
-{props.timer.min}:{(props.timer.sec<10)? 
-<Typography style={style}  fontWeight="fontWeightBold" variant="h1" component="h2" className={classes.root}>0</Typography>:
-null} {props.timer.sec}</Typography>
-</section>
-          </div>
+            <Typography id="notification" variant="h5" component="h2">{texts}</Typography>
+      <section id='counter'>
+            <Typography style={styleOrginal} fontWeight="fontWeightBold" variant="h1" component="h2" className={classes.root}>{(props.timer.min<10)?<Typography fontWeight="fontWeightBold" variant="h1" component="h2" className={classes.root}>0</Typography>:null}{props.timer.min}:{(props.timer.sec<10)?<Typography fontWeight="fontWeightBold" variant="h1" component="h2" className={classes.root}>0</Typography>:null}{props.timer.sec}</Typography>
+            <Typography id='blink' style={style} fontWeight="fontWeightBold" variant="h1" component="h2" className={classes.root}>{(props.timer.min<10)?<Typography style={style} fontWeight="fontWeightBold" variant="h1" component="h2" className={classes.root}>0</Typography>:null}{props.timer.min}:{(props.timer.sec<10)?<Typography style={style}  fontWeight="fontWeightBold" variant="h1" component="h2" className={classes.root}>0</Typography>:null}{props.timer.sec}</Typography>
+      </section>
+    </div>
   )
 }
